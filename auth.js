c8ebb7f3547228fcbdf55cedc7170cdf80e88301
yt249 <yt249@gmail.com>
2021-05-06 21:49:19 -0400

    define callback route

--- auth.js
+++ auth.js
@@ -22,3 +22,23 @@ router.get(
     res.redirect("/");
   }
 );
+
+router.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      const returnTo = req.session.returnTo;
+      delete req.session.returnTo;
+      res.redirect(returnTo || "/");
+    });
+  })(req, res, next);
+});
+
