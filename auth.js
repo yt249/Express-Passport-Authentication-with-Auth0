9e06dde2ceb98076cb78380f5a889819457f5060
yt249 <yt249@gmail.com>
2021-05-06 21:49:39 -0400

    define logout route and export module

--- auth.js
+++ auth.js
@@ -42,3 +42,34 @@ router.get("/callback", (req, res, next) => {
   })(req, res, next);
 });
 
+router.get("/logout", (req, res) => {
+  req.logOut();
+
+  let returnTo = req.protocol + "://" + req.hostname;
+  const port = req.connection.localPort;
+
+  if (port !== undefined && port !== 80 && port !== 443) {
+    returnTo =
+      process.env.NODE_ENV === "production"
+        ? `${returnTo}/`
+        : `${returnTo}:${port}/`;
+  }
+
+  const logoutURL = new URL(
+    `https://${process.env.AUTH0_DOMAIN}/v2/logout`
+  );
+
+  const searchString = querystring.stringify({
+    client_id: process.env.AUTH0_CLIENT_ID,
+    returnTo: returnTo
+  });
+  logoutURL.search = searchString;
+
+  res.redirect(logoutURL);
+});
+
+/**
+ * Module Exports
+ */
+
+ module.exports = router;
