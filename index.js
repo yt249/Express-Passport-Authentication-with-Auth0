1504308d2b7a0050eed1302029814999343881d8
yt249 <yt249@gmail.com>
2021-05-06 21:47:17 -0400

    Store and retrieve user data from the session

--- index.js
+++ index.js
@@ -70,6 +70,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(expressSession(session));
 
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
 /**
  * Routes Definitions
  */
