213ac41efef5e5608e95a67cfb2df6ef188e42d7
yt249 <yt249@gmail.com>
2021-05-06 21:45:02 -0400

    require passport, session, auth0 and add .env

--- index.js
+++ index.js
@@ -7,12 +7,29 @@
 const express = require("express");
 const path = require("path");
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
 
 const app = express();
 const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration (New!)
+ */
+
+
+
+/**
+ * Passport Configuration (New!)
+ */
+
 
 /**
  *  App Configuration
